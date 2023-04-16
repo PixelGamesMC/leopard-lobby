fd4cd1c110b31300bdaef162d27de702bcbc01a1
NitrinCloud <admin@nitrin.net>
2023-04-16 14:40:26 +0200

    Teleport player to spawn on join

--- src/main/kotlin/eu/pixelgamesmc/minecraft/lobby/listener/WorldListener.kt
+++ src/main/kotlin/eu/pixelgamesmc/minecraft/lobby/listener/WorldListener.kt
@@ -1,13 +1,23 @@
 package eu.pixelgamesmc.minecraft.lobby.listener
 
+import eu.pixelgamesmc.minecraft.lobby.configuration.LocationsConfiguration
 import eu.pixelgamesmc.minecraft.lobby.player.isBuilding
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntitySpawnEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.Plugin
 
-class WorldListener: Listener {
+class WorldListener(private val plugin: Plugin): Listener {
+
+    @EventHandler
+    fun playerJoin(event: PlayerJoinEvent) {
+        val locationsConfiguration = LocationsConfiguration.getConfiguration(plugin)
+
+        event.player.teleport(locationsConfiguration.spawnLocation)
+    }
 
     @EventHandler
     fun blockPlace(event: BlockPlaceEvent) {
